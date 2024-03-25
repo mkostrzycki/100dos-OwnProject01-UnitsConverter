978530751b8c6a8adb639690afa90f236e296fdd
MaćKo <maciej.kostrzycki@gmail.com>
2024-03-25 02:57:46 +0100

    Initial Commit

---
+++ UnitsConverter/UnitsConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitsConverterApp.swift
+//  UnitsConverter
+//
+//  Created by MaćKo on 25/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitsConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
